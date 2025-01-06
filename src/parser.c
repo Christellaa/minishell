a2ca1b426a599d2adb9aa79d88b2ce29cd79f98e
cde-sous <cde-sous@student.42.fr>
2025-01-06 15:40:15 +0100

    validate pipeline order

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:08:29 by cde-sous          #+#    #+#             */
-/*   Updated: 2024/12/13 14:26:24 by cde-sous         ###   ########.fr       */
+/*   Updated: 2025/01/06 15:12:42 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,59 @@ void	replace_type_word(t_token **token_list)
 	}
 }
 
-int	check_pipeline_order(t_token *token_list)
+int	is_order_valid(t_token *current, t_token *next)
 {
+	if (current->type == ASSIGNMENT || current->type == FILENAME)
+		// impossible to have ARG/FILENAME and everything else is good
+		return (1);
+	else if (current->type == INFILE || current->type == HEREDOC
+		|| current->type == TRUNC || current->type == APPEND)
+		return (next && next->type == FILENAME);
+	else if (current->type == PIPE) // if first token, return 0 because bad
+		return (next && next->type != PIPE);
+	else if (current->type == CMD || current->type == ARG)
+		return (!next || (next->type != CMD && next->type != ASSIGNMENT));
+	return (0);
+}
+
+/*
+ASSIGNMENT || FILENAME:
+	- NULL
+	- CHEVRON
+	- ASSIGNMENT
+	- CMD
+	- PIPE
+CHEVRON:
+	- next is NOT NULL!
+	- FILENAME
+PIPE:
+	- next is NOT NULL!
+	- CHEVRON
+	- ASSIGNMENT
+	- CMD
+CMD || ARG:
+	- NULL
+	- CHEVRON
+	- ARG
+	- PIPE
+*/
+
+int	validate_pipeline(t_token *token_list)
+{
+	t_token	*current;
+
+	current = token_list;
+	while (current)
+	{
+		if (!is_order_valid(current, current->next))
+		{
+			printf("AAAAA\n");
+			return (0);
+		}
+		printf("BBBBB\n");
+		current = current->next;
+	}
+	return (1);
 }
 
 int	parser(t_data *data)
@@ -79,6 +130,30 @@ int	parser(t_data *data)
 						// error if it was just "" or '' (=> cmd not found)
 	replace_type_word(&data->token_list);
 	// check if order of pipeline is valid
-	check_pipeline_order(data->token_list);
+	if (!validate_pipeline(data->token_list))
+		return (-1); // invalid pipeline
 	return (0);
 }
+
+/*
+TODO:
+1:
+	redo replace_type_word for:
+		when ASSIGNMENT is after CMD/ARG => replace it as ARG
+		eg: ls a=b
+		eg2: ls a=b -l
+		eg3: ls -l a=b
+
+	then modify is_order_valid of validate_pipeline of parser:
+		for if current type is CMD or ARG => delete returning for ASSIGNMENT bc no longer ASSIGNMENT after CMD,
+			only ARG
+
+2:
+	in is_order_valid in validate_pipeline,
+		make sure PIPE isn't the first token, otherwise bad pipeline
+3:
+	clean up every function by:
+		- deleting useless parts (eg: because they're reused during another part)
+		- make more meaningful names
+		- compact the function
+*/
