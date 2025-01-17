b7adad9dbc4b87feb13de1a7ea2b2fcdec3cd0d2
Christella De Sousa <cde-sous@paul-f4Ar4s2.clusters.42paris.fr>
2025-01-17 17:35:48 +0100

    Fix function name

--- src/signals.c
+++ src/signals.c
@@ -6,13 +6,13 @@
 /*   By: cde-sous <cde-sous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:30:18 by cde-sous          #+#    #+#             */
-/*   Updated: 2025/01/17 17:15:45 by cde-sous         ###   ########.fr       */
+/*   Updated: 2025/01/17 17:35:23 by cde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	sigInt(int code)
+void	sig_int(int code)
 {
 	(void)code;
 	printf("\nminishell$ ");
@@ -21,6 +21,6 @@ void	sigInt(int code)
 
 void	handle_signals(void)
 {
-	signal(SIGINT, sigInt);
+	signal(SIGINT, sig_int);
 	signal(SIGQUIT, SIG_IGN);
 }
